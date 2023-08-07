30bc6650ccc7dc3506743c4d5affd92444c75f14
Lavesh Khairajani <115465151+LaveshK11@users.noreply.github.com>
2023-08-07 17:30:58 +0530

    updated code

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import mongoConnect from './src/connection/connection.js'
 import { client } from './src/configuration/bot.js';
 import { verifyUser} from './src/commands/commands.js'
 import cronRun from './src/mailing/emailSender.js'
+import { appendFile } from "fs";
 client.on('ready', async () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -14,8 +15,9 @@ client.login(process.env.TOKEN).then(() => {
     verifyUser()
 })
 
-
-
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
 
 
 
